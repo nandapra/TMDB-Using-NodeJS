5074f523ee0f19edfb36f38ecfcf8065ca4a4a43
Nanda Cahyangtia Pratama <private.nandapra@gmail.com>
2022-05-14 18:46:39 +0700

    add endpoint

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const env = require("dotenv");
+env.config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const movieRouter = require('./routes/movie');
 
 var app = express();
 
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/v1', movieRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
